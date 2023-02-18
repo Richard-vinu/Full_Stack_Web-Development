46bc1fab1ea5c617e081fc900f9b75132a235575
Richard_it <78643671+Richard-vinu@users.noreply.github.com>
2023-02-18 22:47:21 +0530

    Create Nest.js

---
+++ Nest.js
@@ -0,0 +1,11 @@
+npm i -g @nestjs/cli
+nest new project-name
+
+Alternatives
+Alternatively, to install the TypeScript starter project with Git:
+
+git clone https://github.com/nestjs/typescript-starter.git project
+cd project
+npm install
+
+nestJS start
