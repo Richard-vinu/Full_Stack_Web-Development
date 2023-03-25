1a2faaa1c100ef7c1f5bc53c595bfb0323a47f4a
Richard_it <78643671+Richard-vinu@users.noreply.github.com>
2023-03-25 11:16:20 +0530

    Update Nest.js

--- Nest.js
+++ Nest.js
@@ -1,15 +1,1 @@
-npm i -g @nestjs/cli
-nest new project-name
 
-Alternatives
-Alternatively, to install the TypeScript starter project with Git:
-
-git clone https://github.com/nestjs/typescript-starter.git project
-cd project
-npm install
-
-nestJS start
-
-What is NestJS used for?
-Nest. js uses TypeScript for type checking and provides an out-of-the-box software architecture for building and deploying testable, <br>
-  scalable, loosely coupled, and easily maintainable applications. In this article, we'll explore Nest. js and what you can build with it.
